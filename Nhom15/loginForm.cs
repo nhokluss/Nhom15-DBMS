4c2bbc9232b70416ef9f65cd130561cc39e97b7e
Luss <nhok8rytaband@gmail.com>
2022-12-30 21:13:19 +0700

    Add project files.

---
+++ Nhom15/loginForm.cs
@@ -0,0 +1,20 @@
+namespace Nhom15
+{
+    public partial class loginForm : Form
+    {
+        public loginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
